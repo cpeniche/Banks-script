680eb8fe5560f837fae2751dd3e2ee38d35237d9
Carlo <cpeniche777@hotmail.com>
2024-01-26 23:19:55 -0800

    Wells Fargo Statements
    Table detection coordinates needs to be improve

--- table_parser/main.py
+++ table_parser/main.py
@@ -6,6 +6,8 @@ import tabula
 from tabula import read_pdf
 from tabulate import tabulate
 import pandas
+from builtins import isinstance
+from pathlib import Path
  
 #reads table from pdf file
 
@@ -23,27 +25,77 @@ months ={"Jan":"01",
          "Dec":"12",
         }
 
+
+directory = sys.argv[1]
 new_stm=""
 area = [70, 30, 750, 570]
+pathlist = Path(sys.argv[1]).rglob('*.pdf')
 
-df_list = read_pdf(sys.argv[1], guess=False, lattice=False, stream=True, 
-              multiple_tables=False, area=area, pages="2") 
-
-
-stmt = df_list[0]
-
-stmt=stmt.drop(columns='Unnamed: 3').to_string()
-#print(stmt)
-for line in stmt.split('\n'):    
-    if data:=re.search(r'\b[0-9]+\s+[0-9]{1,2}/[0-9]{1,2}.*',line):        
-        new_stm += data.group(0) + '\n'
-ss=""
-for line in new_stm.split('\n'): 
-    ss += re.sub(r'\A[0-9]+\s+','',line) + '\n'
+for path in pathlist:
+    df_list = read_pdf(str(path), guess=False, lattice=False, stream=True, 
+                  multiple_tables=False, area=area, pages="2") 
     
-ss= re.sub(r'NaN',r'',ss)
+    
+    
+    stmt = df_list[0]
+    print(str(path)+'\n')
+    print(stmt)
+    print('**************************')
+    stmt.reset_index()
+    minus=""
+    
+    #Remove unused column.
+    stmt=stmt.drop(stmt.columns[[3]],axis=1)
+     
+    for index,rows in stmt.iterrows():
+        if isinstance(rows[0],str):  
+            #look for row with date 
+            if re.search(r'\A[0-9]{1,2}/[0-9]{1,2}',rows[0])==None:
+                stmt=stmt.drop(index=index)  
+            else:
+                # Add year to the date
+                stmt.loc[index][0]=re.sub(r'(?P<dates>\A[0-9]{1,2}/[0-9]{1,2})','\g<dates>/2019,',rows[0])
+                for i in range(1,3):
+                    if isinstance(rows[i],str): 
+                        #Remove coma from the numbers
+                        rows[i] = re.sub(r',','',rows[i])
+                        minus=""
+                        #Numbers in the second column should be negative
+                        if i == 2:
+                            minus="-"                        
+                        stmt.loc[index][i] = re.sub(r'(?P<numbers>[0-9]*[0-9]+\.[0-9]*)'
+                                                    ,rf',{minus}$\g<numbers>',rows[i])
+                    
+                    
+                                        
+        else:
+            stmt=stmt.drop(index=index)
+    
+    file_data=""
+    
+    output=re.sub(r'\bUnnamed.*',"",stmt.to_string(index=False)) 
+    output=re.sub(r'NaN',"",output)
+    for line in output.split('\n'):
+        file_data += re.sub(r'^\s+',"",line) +'\n' 
+   
+    ofile = open(str(path)+'.csv','w')  
+    ofile.write(file_data) 
+    ofile.close()    
+             
+   
+#===============================================================================
+#     if data:=re.search(r'\b[0-9]+\s+[0-9]{1,2}/[0-9]{1,2}.*',line):        
+#         new_stm += data.group(0) + '\n'
+#     else
+#         stmt.drop(index=)
+# ss=""
+# for line in new_stm.split('\n'): 
+#     ss += re.sub(r'\A[0-9]+\s+','',line) + '\n'
+#     
+# ss= re.sub(r'NaN',r'',ss)
+#===============================================================================
 
-print(ss)
+#print(ss)
 #===============================================================================
 # ofile = open('output.txt','w')
 # 
