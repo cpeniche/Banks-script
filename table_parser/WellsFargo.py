5ea5730f2fb2cd3c1fb3699340d847d53ac2dcaa
Carlo <cpeniche777@hotmail.com>
2024-01-31 21:04:40 -0800

    Two page table reads correctly

--- table_parser/WellsFargo.py
+++ table_parser/WellsFargo.py
@@ -13,127 +13,144 @@ import fitz
 
 
 class Statement(object):
+
     
+  columns=[]         
+  init_table_mark_position=[]
+  end_table_mark_position=[]
+  columns_to_drop=["col_4"]
+  find_date=r'\A[0-9]{1,2}/[0-9]{1,2}'
+  find_numbers=r'[0-9]*[0-9]+\.[0-9]*'
+  raw_table_list=[]
+  new_table_list=[]
+  stm_as_string =""
+  init_table_mark = "Transaction history"
+  end_table_mark ="Totals"
   
-    columns=[]         
-    init_table_mark_position=[]
-    end_table_mark_position=[]
-    columns_to_drop=["col_4"]
-    find_date=r'\A[0-9]{1,2}/[0-9]{1,2}'
-    find_numbers=r'[0-9]*[0-9]+\.[0-9]*'
-    raw_stmt=pd.DataFrame()
-    new_stmt=pd.DataFrame()
-    stm_as_string =""
-    init_table_mark = "Transaction history"
-    end_table_mark ="Totals"
+  
+  def __init__(self, date):
+    self.date = date
+      
+  
+  def open(self,file): 
+      
+    pdf_document = fitz.open(file)
     
-    
-    def __init__(self, date):
-        self.date = date
-        
-    
-    def open(self,file): 
-        
-        pdf_document = fitz.open(file)
-        
-        #look for first marker on pages, if the table is bigger than one page two or more
-        #markers will be find         
-        for page in pdf_document:
-            text_instances = page.search_for(self.init_table_mark)        
-            for text_instance in text_instances:
-                self.init_table_mark_position.append({"page":page.number,
-                                                      "x0":int(text_instance.x0),
-                                                      "y0":int(text_instance.y0),
-                                                      "x1":int(page.mediabox_size.x),
-                                                      "y1":int(page.mediabox_size.y)
-                                                      })
-                
-        #look for the unique end marker (hopefully)                                                    
-        for page in pdf_document:
-            text_instances = page.search_for(self.end_table_mark)
-            for text_instance in text_instances:
-                self.end_table_mark_position.append({"page":page.number,"x0":int(text_instance.x0),"y0":int(text_instance.y0)})
-                
-        self.init_table_mark_position[-1]["y1"] = self.end_table_mark_position[0]["y0"]    
-                                        
-        #Open raw pdf file on the specified area
-        for table in self.init_table_mark_position:       
-            self.raw_stmt=read_pdf(file, guess=False, lattice=False, stream=True,multiple_tables=False,
-                                   area = [table["y0"], table["x0"], table["y1"], table["x1"]], pages = table["page"]+1)[0]
-            print(self.raw_stmt)
-                       
-        #print(self.raw_stmt.to_string())
-                       
+    #look for first marker on pages, if the table is bigger than one page two or more
+    #markers will be find         
+    for page in pdf_document:
+      text_instances = page.search_for(self.init_table_mark)        
+      for text_instance in text_instances:
+          self.init_table_mark_position.append({"page":page.number,
+                                                "x0":int(text_instance.x0),
+                                                "y0":int(text_instance.y0),
+                                                "x1":int(page.mediabox_size.x),
+                                                "y1":int(page.mediabox_size.y)
+                                                })
+          
+    #look for the unique end marker (hopefully)                                                    
+    for page in pdf_document:
+      text_instances = page.search_for(self.end_table_mark)
+      for text_instance in text_instances:
+        self.end_table_mark_position.append({"page":page.number,
+                                               "x0":int(text_instance.x0),
+                                               "y0":int(text_instance.y0)})
+            
+    self.init_table_mark_position[-1]["y1"] = self.end_table_mark_position[0]["y0"]    
+                                    
+    #Open raw pdf file on the specified area
+    for idx,table in enumerate(self.init_table_mark_position):       
+      self.raw_table_list.append({"index":idx, "table": read_pdf(file, 
+                            guess = False, 
+                            lattice=False, 
+                            stream=True,
+                            multiple_tables=False,
+                            area = [table["y0"], table["x0"], table["y1"], table["x1"]], 
+                            pages = table["page"]+1)[0]})
        
-    def parse(self):
         
-        
-        if len(self.raw_stmt.columns) < 3:
-            return False
-        
-        idx=0;
-        #Change name to columns
-        for col in self.raw_stmt.columns:                        
-            self.columns.append("col_"+ str(idx))
-            idx=idx+1
-            
-        self.raw_stmt.columns= self.columns          
-        #print(self.raw_stmt.columns)  
-        
-        for col in self.columns_to_drop:  
-            self.new_stmt=self.raw_stmt.drop(col,axis=1)   
-              
-        #iterate over rows        
-        for index,rows in self.new_stmt.iterrows(): 
-            if isinstance(rows[self.columns[0]],str):  
-            #look for row with date 
-                if re.search(self.find_date,rows[self.columns[0]])==None:
-                    #we drop raw without date information
-                    self.new_stmt=self.new_stmt.drop(index=index)  
-                else:
-                    # Add year to the date
-                    rows[self.columns[1]]=re.sub(r',','',rows[self.columns[1]])
-                    rows[self.columns[1]]=re.sub(r'\A\s*','',rows[self.columns[1]])
-                    #rows[self.columns[1]].lstrip()
-                    rows[self.columns[0]]=re.sub(self.find_date,rf'\g<0>/{self.date},',rows[self.columns[0]])
-                    self.new_stmt.loc[index]["col_0"]=rows[self.columns[0]]
-                    self.new_stmt.loc[index]["col_1"]=rows[self.columns[1]]
-                    for i in itertools.islice(self.columns,2,len(self.columns)-1) :                    
-                        if isinstance(rows[i],str): 
-                            #Remove coma from the numbers
-                            rows[i] = re.sub(r',','',rows[i])
-                            minus="-"
-                            #Numbers in the credit column should be negative
-                            if i == "col_2":
-                                minus=""                        
-                            self.new_stmt.loc[index][i] = re.sub(self.find_numbers
-                                                        ,rf',{minus}$\g<0>',rows[i])
-                    
-                        
-                                        
-            else:
-                self.new_stmt=self.new_stmt.drop(index=index)
-                
-        return True
-            
-            
-    def get_string(self):
-        if self.new_stmt.empty == False:
-            temp_str=re.sub(r'\bcol.*\n',"",self.new_stmt.to_string(index=False)) 
-            temp_str=re.sub(r'NaN',"",temp_str)
-            for line in temp_str.split('\n'):
-                self.stm_as_string += re.sub(r'^\s+',"",line)+'\n'
-            return self.stm_as_string
-        else:
-            return ""       
-            
-        
-    def clear(self):
-        self.raw_stmt[0:0]
-        self.new_stmt[0:0]  
-        self.columns.clear()
-        self.stm_as_string=''
+    # for table in self.raw_table_list: 
+    #   print(table["table"])
     
+                   
+   
+                         
+  def parse(self):
+      
+    new_table=[]
+      
+    for tbl_idx, table in enumerate(self.raw_table_list):  
+      if len(table["table"].columns) < 3:
+        return False
+            
+      #Change name to columns
+      for idx,col in enumerate(table["table"].columns):                        
+        self.columns.append("col_"+ str(idx))
+                    
+      table["table"].columns= self.columns          
+      #print(self.table.columns)  
+      
+      for col in self.columns_to_drop:  
+        new_table.append(table["table"].drop(col,axis=1))   
+            
+      #iterate over rows        
+      for index,rows in new_table[tbl_idx].iterrows(): 
+        if isinstance(rows[self.columns[0]],str):  
+        #look for row with date 
+            if re.search(self.find_date,rows[self.columns[0]])==None:
+                #we drop raw without date information
+                new_table[tbl_idx]=new_table[tbl_idx].drop(index=index)  
+            else:
+                # Add year to the date
+                rows[self.columns[1]]=re.sub(r',','',rows[self.columns[1]])
+                rows[self.columns[1]]=re.sub(r'\A\s*','',rows[self.columns[1]])
+                
+                rows[self.columns[0]]=re.sub(self.find_date,rf'\g<0>/{self.date},',rows[self.columns[0]])
+                new_table[tbl_idx].loc[index]["col_0"]=rows[self.columns[0]]
+                new_table[tbl_idx].loc[index]["col_1"]=rows[self.columns[1]]
+                for i in itertools.islice(self.columns,2,len(self.columns)-1) :                    
+                    if isinstance(rows[i],str): 
+                        #Remove coma from the numbers
+                        rows[i] = re.sub(r',','',rows[i])
+                        minus="-"
+                        #Numbers in the credit column should be negative
+                        if i == "col_2":
+                            minus=""                        
+                        new_table[tbl_idx].loc[index][i] = re.sub(self.find_numbers
+                                                    ,rf',{minus}$\g<0>',rows[i])
+                
+                    
+                                    
+        else:
+          new_table[tbl_idx]=new_table[tbl_idx].drop(index=index)
+        
+      self.new_table_list.append(new_table[tbl_idx])
+      self.columns.clear()
+            
+    return True
+          
+          
+  def get_string(self):
+    
+    for table in self.new_table_list:
+      if table.empty == False:
+        temp_str=re.sub(r'\bcol.*\n',"",table.to_string(index=False)) 
+        temp_str=re.sub(r'NaN',"",temp_str)
+        for line in temp_str.split('\n'):
+          self.stm_as_string += re.sub(r'^\s+',"",line)+'\n'                
+      else:
+        return ""      
+    return self.stm_as_string 
+          
+      
+  def clear(self):
+    self.raw_table_list.clear()
+    self.new_table_list.clear()    
+    self.columns.clear()
+    self.stm_as_string=''
+    self.init_table_mark_position.clear()
+    self.end_table_mark_position.clear()
+  
   
   
     
--- table_parser/table_parser.py
+++ table_parser/table_parser.py
@@ -13,29 +13,29 @@ if __name__ == '__main__':
     year = sys.argv[2]
     file = sys.argv[1]
     if len(sys.argv) == 4:
-        if sys.argv[3] == '-batch':            
-            pathlist = Path(sys.argv[1]).rglob('*.pdf')                   
-            for file in pathlist :
-                print("-----------" + str(file) + "---------------")     
-                Stmt = WF.Statement(year)
-                Stmt.open(str(file))
-                if Stmt.parse() == True:
-                    file_data += Stmt.get_string()
-                else:
-                    file_data += "\n**************** Error at file : "+ str(file) +"*****************\n"
-                #print(file_data)
-                Stmt.clear()
-            ofile = open(sys.argv[1]+"/"+str(year)+'_year_end.csv','w')  
-            ofile.write(file_data) 
-            ofile.close()   
-    else :
-        Stmt = WF.Statement(year)
-        Stmt.open(file)
-        Stmt.parse()        
-        file_data=Stmt.get_string()
-        ofile = open(sys.argv[1] +'.csv','w')  
+      if sys.argv[3] == '-batch':            
+        pathlist = Path(sys.argv[1]).rglob('*.pdf')                   
+        for file in pathlist :
+          print("-----------" + str(file) + "---------------")     
+          Stmt = WF.Statement(year)
+          Stmt.open(str(file))
+          if Stmt.parse() == True:
+            file_data += Stmt.get_string()
+          else:
+            file_data += "\n**************** Error at file : "+ str(file) +"*****************\n"
+          #print(file_data)
+          Stmt.clear()
+        ofile = open(sys.argv[1]+"/"+str(year)+'_transactions.csv','w')  
         ofile.write(file_data) 
-        ofile.close()  
+        ofile.close()   
+    else :
+      Stmt = WF.Statement(year)
+      Stmt.open(file)
+      Stmt.parse()        
+      file_data=Stmt.get_string()
+      ofile = open(sys.argv[1] +'.csv','w')  
+      ofile.write(file_data) 
+      ofile.close()  
     
     print("Parser done")   
     
